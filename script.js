535e687c2538bcc6e1cefa2ed8cbafebcef2c595
Alexander Dyatlov <coalaru@gmail.com>
2023-05-21 17:59:58 +0300

    added HW-2.4

--- script.js
+++ script.js
@@ -1,101 +1,59 @@
 
 //Задание 1
-let password = `юзер`;
-let userPass = prompt(`Введите пароль`);
-if (password === userPass) {
-    alert(`Пароль введён верно`);
-} else {
-    alert(`Пароль введён неверно`);
+let i = 1;
+while (i <= 2) {
+    console.log(`Привет!`);
+    i++;
 }
 
-
 //Задание 2
-let x = prompt(`Введите число`);
-if (0 < x && x < 10) {
-    alert(`Верно`);
-} else {
-    alert(`Неверно`);
+let i = 1;
+while (i <= 5) {
+    console.log(i);
+    i++;
 }
 
 
 //Задание 3;
-let d = prompt(`Введите число 1`);
-let e = prompt(`Введите число 2`);
-if (d > 100 || e > 100) {
-    alert(`Верно`);
-} else {
-    alert(`Неверно`);
+let i = 7;
+while (i <= 22) {
+    console.log(i);
+    i++;
 }
 
 //Задание 4
-let a = `2`;
-let b = `3`;
-alert(Number(a) + Number(b));
+const obj = {
+    'Коля': '200',
+    'Вася': '300',
+    'Петя': '400'
+}
+for (let name in obj) {
+    alert(`${name} -зарплата ${obj[name]}`);
+}
 
 //Задание 5
-let monthNumber = prompt(`Введите номер месяца`);
-switch (monthNumber) {
-    case '12':
-    case '1':
-    case '2':
-        alert(`Зима`);
-        break;
-    case '3':
-    case '4':
-    case '5':
-        alert(`Весна`);
-        break;
-    case '6':
-    case '7':
-    case '8':
-        alert(`Лето`);
-        break;
-    case '9':
-    case '10':
-    case '11':
-        alert(`Осень`);
-        break;
-    default:
-        break;
-}
+let num = 0;
+let n = 1000;
+do {
+    n = n / 2;
+    num++;
+} while (n >= 50);
+console.log(num);
 
+//Задание 6
+//Май 2023, 31 день в месяце
+const firstFriday = 5;
+const monthLenght = 31;
 
-//Задание 7
-let module;
-let z = prompt(`Введите число`);
-if (isNaN(z) === false) { module = (z % 2) }
-else { alert(`Вы ввели НЕ ЧИСЛО`) };
-if (module === 0) { alert(`Число четное`) }
-if (module === 1) { alert(`Число нечетное`) };
+for (let dateFriday = firstFriday; dateFriday <= monthLenght; dateFriday = dateFriday + 7) {
+    alert(`Сегодня пятница, ${dateFriday}-е число. Необходимо подготовить отчет.`)
+};
 
-//Задание 8
-let clientOS = (Number(prompt(`Выберете вариант 0-iOS, 1-Android`)));
-if (clientOS == 0) { alert(`Установите версию приложения для iOS по ссылке`); }
-if (clientOS == 1) { alert(`Установите версию приложения для Android по ссылке`); }
+//Задание 6.1
+//для любого месяца без констант
+let d = new Date();
+let date = d.getDate();
+let msg = d.getDay();
 
-
-
-//Задание 9
-const deadLineYear = 2015;
-let clientDeviceYear = (Number(prompt(`введите год производства Вашего телефона`)));
-switch (clientOS) {
-    case 0:
-        if (clientDeviceYear >= deadLineYear) {
-            alert(`Установите обычную версию приложения для iOS по ссылке`);
-        } else { alert(`Установите облегчённую версию приложения для iOS по ссылке`); }
-        break;
-    case 1:
-        if (clientDeviceYear >= deadLineYear) {
-            alert(`Установите обычную версию приложения для Android по ссылке`);
-        } else { alert(`Установите облегчённую версию приложения для Android по ссылке`); }
-        break;
-    default:
-        alert(`ошибка ввода`);
-
-}
-
-//Задание 9 иной вариант
-if (clientDeviceYear >= deadLineYear && clientOS == 0) { alert(`Установите обычную версию приложения для iOS по ссылке`); }
-if (clientDeviceYear >= deadLineYear && clientOS == 1) { alert(`Установите обычную версию приложения для Android по ссылке`); }
-if (clientDeviceYear < deadLineYear && clientOS == 0) { alert(`Установите облегчённую версию приложения для iOS по ссылке`); }
-if (clientDeviceYear < deadLineYear && clientOS == 1) { alert(`Установите облегчённую версию приложения для Android по ссылке`); }
+if (msg == 6) { alert(`Пятница! ${date}-е число! Подготовить отчёт`); }
+else { alert(`Пока отчет не нужен`); }
